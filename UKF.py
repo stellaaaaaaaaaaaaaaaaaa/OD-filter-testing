705f84c276f444778aa5895e4e1fdc097d46a712
Stella <stellaedmondston@gmail.com>
2025-04-08 12:55:24 +1000

    Add files via upload

--- UKF.py
+++ UKF.py
@@ -10,7 +10,7 @@ def run_unscented_kalman_filter(XREF, tk, Rk, Qd):
     
     filter_results = np.zeros_like(XREF)
     covariance_results = np.zeros((XREF.shape[0],1))
-    state_dev_results = np.zeros_like(XREF)
+    
     
     for i, row in enumerate(XREF):
     
@@ -31,8 +31,8 @@ def run_unscented_kalman_filter(XREF, tk, Rk, Qd):
         Wim = 1/(2*(L+lbda))
         Wic = Wim
         
+        
         #step 2: initialisation (a priori)
-        tkminus1 = tk[i] #time at last observation
         XREFminus1 = XREF[i] #previous reference state determined from last read
         
         if len(covariance_results) == 0:
@@ -43,9 +43,8 @@ def run_unscented_kalman_filter(XREF, tk, Rk, Qd):
                 Pkminus1 = 0
             else:
                 Pkminus1 = covariance_results[i-1] #previous covariance/uncertainty determined from last read
-         
-        XREF0 == XREFkminus1 #current reference state
-        P0 == Pkminus1 #covariance set by you
+        
+        tkminus1 = tk[i]
         tk = tk[i+1]
  
         
@@ -53,82 +52,106 @@ def run_unscented_kalman_filter(XREF, tk, Rk, Qd):
         Proot = np.linalg.cholesky(Pkminus1, lower=True)
         sigma_matrix = np.zeros((L, 2*L+1)) #empty matrix
         
+        sigma_matrix[:, 0] = XREFminus1 #first sigma point
+        
         for i in range(L):
-            sigma_matrix[:, i+1] = XREFminus1 + gamma*Proot[:, i]
+            sigma_matrix[:, i+1] = XREFminus1 + gamma*Proot[:, i] #mean + scaled root of covariance
             
         for i in range(L):
-            sigma_matrix[:, i+L+1] = XREFminus1 - gamma*Proot[:, i]
+            sigma_matrix[:, i+L+1] = XREFminus1 - gamma*Proot[:, i] #mean - scaled root of covariance
         
         
-        #step 4: read next observation 
-        #use CR3BP function to obtain XREFk (current reference state) and STM(tk, tk-1)
-        NRHOmotion(XREF0, P0, tkminus1, tk, Rk)
-        
-        
-        #step 5: time update !!
+        #step 4: time update !!
         #this is different because each sigma point (13) is propagated with the equations of motion
-        xk = np.zeros((L, 2*L+1)) #empty matrix
+        xbark = np.zeros((L, 2*L+1)) #empty matrix
         for i in range(L):
-            xk[:, i+1] = NRHOmotion(sigma_matrix[:, i], P0, tkminus1, tk, Rk)
+            xbark[:, i+1] = NRHOmotion(sigma_matrix[:, i], Pkminus1, tkminus1, tk)
             
         
-        #step 6: process noise step
-        #compute a priori state xk and covariance Pk for tk
+        #step 5: process noise step
+        #compute a state xk and covariance Pk for tk
         Xksigma = np.zeros((L, 2*L+1))
         for i in range(2*L):
-            Xksigma = Wim*xk[:,i]
+            if i == 0:
+                Xksigma[:,i] = W0m*xbark[:,i]
+            else:
+                Xksigma[:,i] = Wim*xbark[:,i]
         
         shape = (6,1)
-        Xk = np.zeros(shape) #initialise
-        Xk = np.sum(Xksigma, axis=1)
+        Xbark = np.zeros(shape) #initialise
+        Xbark = np.sum(Xksigma, axis=1)
         
         #process noise covariance
         
         Pksigma = np.zeros((L, 2*L+1))
         for i in range(2*L):
-            Pksigma[:, i] = Wic*np.dot(self, np.subtract(sigma_matrix, Xk), np.subtract(sigma_matrix, Xk)) + Qd
+            if i == 0:
+                Pksigma[:, i] = W0c*np.dot(np.subtract(xbark[:,i], Xbark), np.transpose(np.subtract(xbark[:,i], Xbark)))
+            else:
+                Pksigma[:, i] = Wic*np.dot(np.subtract(xbark[:,i], Xbark), np.transpose(np.subtract(xbark[:,i], Xbark)))
             
         shape = (6,1)
-        Pk = np.zeros(shape) #initialise
-        Pk = np.sum(Pksigma, axis=1)
+        Pbark = np.zeros(shape) #initialise
+        Pbark = np.sum(Pksigma, axis=1) + Qd
         
-        Prootnew = np.linalg.cholesky(Pk, lower=True)
+        Prootnew = np.linalg.cholesky(Pbark, lower=True)
         sigma_matrix_new = np.zeros((L, 2*L+1)) #empty matrix
         
+        sigma_matrix_new[:, 0] = Xbark #first sigma point
+        
         for i in range(L):
-            sigma_matrix_new[:, i+1] = Xk + gamma*Prootnew[:, i]
+            sigma_matrix_new[:, i+1] = Xbark + gamma*Prootnew[:, i] #mean + scaled root of covariance
             
         for i in range(L):
-            sigma_matrix_new[:, i+L+1] = Xk - gamma*Prootnew[:, i]
+            sigma_matrix_new[:, i+L+1] = Xbark - gamma*Prootnew[:, i] #mean - scaled root of covariance
+                
         
-        
-        #step 7: predicted measurement
+        #step 6: predicted measurement
         #use the UT to compute
-        G == sigma_matrix_new #assuming full state is observed
-        upsilon == G #just maintaining equation conventions
-        H = np.eye(6) #observation mapping matrix
+        upsilon = np.zeros((L, 2*L+1)) #empty matrix
+        for i in range(L):
+            upsilon[:, i+1] = NRHOmotion(sigma_matrix_new[:, i], Pkminus1, tkminus1, tk)
+        
+        Ybarksigma = np.zeros((L, 2*L+1))
         
         for i in range(2*L):
-            Ybarksigma = Wim*upsilon[:, i]
+            if i == 0:
+                Ybarksigma[:,i] = W0m*upsilon[:, i]
+            else:
+                Ybarksigma[:,i] = Wim*upsilon[:, i]
             
         shape = (6,1)
         Ybark = np.zeros(shape) #initialise
         Ybark = np.sum(Ybarksigma, axis=1)
         
-        #step 8: innovation and cross-correlation
-        for i in range(2*L):
-            Pyy = Rk + np.sum(Wic*np.dot(np.subtract(upsilon, Ybark), np.transpose(np.subtract(upsilon, Ybark))), axis=1) #innovation
-            Pxy = np.sum(Wic*np.dot(np.subtract(sigma_matrix, Xk), np.transpose(np.subtract(upsilon, Ybark))), axis=1) #cross-correlation
         
-        #step 9: corrector update
+        #step 7: innovation and cross-correlation
+        
+        innovation_op = np.zeros((L, 2*L+1))
+        crosscor_op = np.zeros((L, 2*L+1))
+        
+        for i in range(2*L):
+            if i == 0: 
+                innovation_op[:, i] = W0c*np.dot(np.subtract(upsilon[:,i], Ybark), np.transpose(np.subtract(upsilon[:,i], Ybark)))
+                crosscor_op[:, i] = W0c*np.dot(np.subtract(sigma_matrix[:,i], Xbark), np.transpose(np.subtract(upsilon[:,i], Ybark)))
+            else:
+                innovation_op[:, i] = Wic*np.dot(np.subtract(upsilon[:,i], Ybark), np.transpose(np.subtract(upsilon[:,i], Ybark)))
+                crosscor_op[:, i] = Wic*np.dot(np.subtract(sigma_matrix[:,i], Xbark), np.transpose(np.subtract(upsilon[:,i], Ybark)))
+            
+            Pyy = Rk + np.sum(innovation_op, axis=1) #innovation
+            Pxy = np.sum(crosscor_op, axis=1) #cross-correlation
+        
+        
+        #step 8: corrector update
+        Yk = XREF[i+1]
         Kk = Pxy @ np.linalg.inv(Pyy) #Kalman gain
-        Xhatk = Xk + Kk @ np.subtract(Yk, Ybark) #final state estimation
+        Xk = Xbark + Kk @ np.subtract(Yk, Ybark) #final state estimation
         Pk = Pbark - Kk @ Pyy @ np.transpose(Kk) #final covariance
          
         #repeat for next observation
         #need to store data in an array such that the filter and the truth data can be plotted against each other
         
-        filter_results[i] = Xhatk
+        filter_results[i] = Xk
         covariance_results[i] = Pk
     
     return filter_results, covariance_results
