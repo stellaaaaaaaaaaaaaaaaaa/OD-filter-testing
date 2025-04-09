a91739da648a1581101b89834b24d9831fa434ef
Stella <stellaedmondston@gmail.com>
2025-04-09 11:36:22 +1000

    Add files via upload

--- CKF.py
+++ CKF.py
@@ -16,6 +16,7 @@ def run_cubature_kalman_filter(XREF, tk, Rk, Qd):
         nx = 6
         Wi = 1/(2*nx)
         
+        
         #step 2: initialisation (a priori)
         tkminus1 = tk[i] #time at last observation
         XREFminus1 = XREF[i] #previous reference state determined from last read
@@ -29,81 +30,92 @@ def run_cubature_kalman_filter(XREF, tk, Rk, Qd):
             else:
                 Pkminus1 = covariance_results[i-1] #previous covariance/uncertainty determined from last read
         
-        XREF0 == XREFkminus1 #current reference state
-        P0 == Pkminus1 #covariance set by you
         tk = tk[i+1]
         
-        #step 3: compute sigma point matrix, Proot via Cholesky
+        
+        #step 3: compute cubature point matrix, Proot via Cholesky
         Proot = np.linalg.cholesky(Pkminus1, lower=True)
-        sigma_matrix = np.zeros((nx, nx+1)) #empty matrix
+        cub_matrix = np.zeros((nx, 2*nx+1)) #empty matrix
+        
+        cub_matrix[:, 0] = XREFminus1
         
         for i in range(nx):
-            sigma_matrix[:, i] = XREFminus1 + np.sqrt(nx)*Proot[:, i]
+            cub_matrix[:, i] = XREFminus1 + np.sqrt(nx)*Proot[:, i]
             
         for i in range(nx):
-            sigma_matrix[:, i+nx] = XREFminus1 - np.sqrt(nx)*Proot[:, i]
-        
-        
-        #step 4: read next observation 
-        #use CR3BP function to obtain XREFk (current reference state) and STM(tk, tk-1)
-        NRHOmotion(XREF0, P0, tkminus1, tk, Rk)
+            cub_matrix[:, i+nx] = XREFminus1 - np.sqrt(nx)*Proot[:, i]
         
         
         #step 5: time update !!
         #this is different because each sigma point (13) is propagated with the equations of motion
-        xksigma = np.zeros((nx, 2*nx+1)) #empty matrix
-        for i in range(2*nx):
-            xksigma[:, i+1] = Wi*NRHOmotion(sigma_matrix[:, i], P0, tkminus1, tk, Yk, Rk)
+        xkcub = np.zeros((nx, 2*nx+1)) #empty matrix
         
+        for i in range(2*nx):
+            xkcub[:, i+1] = Wi*NRHOmotion(cub_matrix[:, i], Pkminus1, tkminus1, tk)
+        
+
+        #step 6: process noise step
+        #sum weighted propagated points to obtain time updated state estimate
         shape = (6,1)
         xk = np.zeros(shape) #initialise
-        xk = np.sum(xksigma, axis=1)
+        xk = np.sum(xkcub, axis=1)
         
-        #step 6: process noise step
-        
-        Pksigma = np.zeros((L, 2*L+1))
+        Pksigma = np.zeros((nx, 2*nx+1))
         for i in range(2*nx):
-            Pksigma[:, i] = xksigma[:, i]/Wi - np.dot(xk, np.transpose(np.subtract(NRHOmotion(sigma_matrix[:, i], P0, tkminus1, tk, Yk, Rk), xk)))*Wi + Qd
+            Pksigma[:, i] = np.dot(np.subtract(xkcub[:, i]/Wi, xk), np.transpose(np.subtract(xkcub[:, i]/Wi, xk))*Wi)
             
         shape = (6,1)
         Pk = np.zeros(shape) #initialise
-        Pk = np.sum(Pksigma, axis=1)
+        Pk = np.sum(Pksigma, axis=1) + Qd
         
         Prootnew = np.linalg.cholesky(Pk, lower=True)
-        sigma_matrix_new = np.zeros((L, 2*L+1)) #empty matrix
+        cub_matrix_new = np.zeros((nx, 2*nx+1)) #empty matrix
+        
+        cub_matrix_new[:, 0] = xk
         
         for i in range(nx):
-            sigma_matrix_new[:, i] = xk + np.sqrt(nx)*Prootnew[:, i]
+            cub_matrix_new[:, i] = xk + np.sqrt(nx)*Prootnew
             
         for i in range(nx):
-            sigma_matrix_new[:, i+nx] = xk - np.sqrt(nx)*Prootnew[:, i]
+            cub_matrix_new[:, i+nx] = xk - np.sqrt(nx)*Prootnew
         
         
         #step 7: predicted measurement
         #use the cubature rule to compute
         h = np.eye(6) #measurement mapping matrix, identity matrix because we know the state (no need to convert from measurement data)
         
+        upsilon = np.zeros((nx, 2*nx+1)) #empty matrix   
+        
         for i in range(2*nx):
-            Ybarksigma = Wi* h @ sigma_matrix_new[:, i]
+            upsilon[:,i+1] = h @ NRHOmotion(cub_matrix_new[:, i], Pkminus1, tkminus1, tk)
             
         shape = (6,1)
         Ybark = np.zeros(shape) #initialise
-        Ybark = np.sum(Ybarksigma, axis=1)
+        Ybark = Wi*np.sum(upsilon, axis=1)
+        
         
         #step 8: innovation and cross-correlation
         for i in range(2*nx):
-            Pyy = Rk + np.sum(Wi*np.dot(np.subtract(h @ sigma_matrix_new[:, i], Ybark[:,i]), np.transpose(np.subtract(h @ sigma_matrix_new[:, i], Ybark[:,i])))) #innovation
-            Pxy = np.sum(Wi*np.dot(np.subtract(sigma_matrix_new[:, i], xk[:,i]), np.transpose(np.subtract(h @ sigma_matrix_new[:, i], Ybark[:,i])))) #cross-correlation
+            innovation_op = np.dot(np.subtract(upsilon[:, i], Ybark), np.transpose(np.subtract(upsilon[:, i], Ybark))) #innovation
+            crosscor_op = np.dot(np.subtract(upsilon[:, i], xk), np.transpose(np.subtract(upsilon[:, i], Ybark))) #cross-correlation
+        
+        Pyy = Rk + Wi*np.sum(innovation_op, axis=1) #innovation
+        Pxy = Wi*np.sum(crosscor_op, axis=1) #cross-correlation
+        
         
         #step 9: corrector update
+        Yk = XREF[i+1]        
+        
         Kk = Pxy @ np.linalg.inv(Pyy) #Kalman gain
-        Xhatk = xk + Kk @ np.subtract(Yk, Ybark) #final state estimation
-        Pk = Pbark - Kk @ np.transpose(Pxy) #final covariance
+        Xkfinal = xk + Kk @ np.subtract(Yk, Ybark) #final state estimation
+        Pkfinal = Pk - Kk @ np.transpose(Pxy) #final covariance
          
         #repeat for next observation
         #need to store data in an array such that the filter and the truth data can be plotted against each other
         
-        filter_results[i] = Xhatk
-        covariance_results[i] = Pk
+        filter_results[i] = Xkfinal
+        covariance_results[i] = Pkfinal
         
     return filter_results, covariance_results
+
+
